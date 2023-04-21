841e94c2c2da01e396d82efd0240499bf426e3b0
Ruth Ramya <bhavani.bolloju@gmail.com>
2023-04-21 16:21:54 +0530

    added hero-section

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -5,7 +5,11 @@ import classes from './HeroSection.module.scss';
 function HeroSection() {
   return (
     <div className={classes['section-hero']}>
-      hero section
+      <div className={classes['section-text-box']}>
+
+      <h1>transforming agriculture with technology</h1>
+      <p className={classes['section-hero-text']}>Revolutionizing Agriculture through Innovation: Explore the Latest Trends and Technologies in Agri-Tech with Ouranos Robotics' Leading Blog</p>
+      </div>
     </div>
   );
 }
